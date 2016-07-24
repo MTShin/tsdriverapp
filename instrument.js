0175ae6adb6e9ecc99a22a1b1ab10b4ba40b79b9
Spin Wang <spinwang525@gmail.com>
2016-07-24 15:47:26 -0400

    initial draft for instrument model

---
+++ instrument.js
@@ -0,0 +1,32 @@
+/**
+ * Created by spin on 7/16/16.
+ */
+
+var mongoose = require('mongoose');
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var mixType  = mongoose.Schema.Types.Mixed;
+
+var InstrumentSchema = new mongoose.Schema({
+
+
+    parent      :       {type: ObjectId, ref: 'Instrument'},
+
+    driver      :       {type: ObjectId, ref: 'Driver'},
+    status      :       {type: String},
+    type        :       {type: String},
+    model       :       {type: mixType},
+    manufacturer:       {type: String},
+    time        :       {type: Date, default: Date.now},
+    tOrg        :       {type: ObjectId, ref: 'Organization'},
+    uOrg        :       [{type: ObjectId, ref: 'Organization'}],
+    price       :       {type: mixType},
+    instruction :       {
+        userManual      :       [{type: String}],
+        tutorial        :       [{type: String}]
+    },
+    dataExtraction  : {type: String, enum: ['command','log','']}
+
+
+});
+
