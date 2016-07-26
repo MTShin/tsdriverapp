16d65262c35b5ce0fe5b754c6f97601ebb287afd
Spin Wang <spinwang525@gmail.com>
2016-07-25 23:03:39 -0400

    inlcude command schema in sensor schema

--- app/models/Driver.js
+++ app/models/Driver.js
@@ -22,19 +22,7 @@ var SerialComSchema = new Mongoose.Schema({
 
 });
 
-/** Sensor **/
-var SensorSchema = new Mongoose.Schema({
-    command_id          : {type: String},
-    command_string      : {type: String},
-    command_params      : [{
-        cmd_param   : {type: String}, // param name, should match exactly with the one used in the command_string 'ab{cmd_param}\r'
-        default_val : MixType,
-        param_min   : {type: Number},
-        param_max   : {type: Number}
-    }],
-    cmd_description     : {type: String},
-    interval            : {type: Number, default: 5, min: 0.8}
-});
+
 
 /** Trigger Action **/
 var TriggerActionSchema = new Mongoose.Schema({
@@ -43,13 +31,49 @@ var TriggerActionSchema = new Mongoose.Schema({
 });
 /** Trigger **/
 var TriggerSchema = new Mongoose.Schema({
-    allow_multiple      :   {type: Boolean, default: true}, // you are allowed to create multiple trigger on temperature feed
+    allow_multiple      :   {type: Boolean, default: true}, // whether it's allowed to create multiple trigger on temperature feed
                                                             // todo: what if trigger is created and then the driver changes, how about legacy triggers
+
     trigger_type        :   {type: String, enum: ['state','numerical']},
     trigger_labels      :   [{value: Number, label: String}],
     trigger_eval        :   {type: String},
-    one_time            :   {type: Boolean, default: false}, // one_time trigger can only be triggered once, i.e, 'alert me when idle'
+    one_time            :   {type: Boolean, default: false}, // one_time trigger can only be triggered once, i.e, 'alert me when idle' (could be overkill)
     trigger_action      :   [TriggerActionSchema]
+});
+
+/** Filter **/
+var FilterSchema = new Mongoose.Schema({
+    regex       :   {type: String},
+    name        :   {typpe: String},
+    // todo: should action contain trigger? this way we can decide whether cycle complete should show up on the trigger modal.
+    // todo: (commonly known as the alert modal, but essentially user is creating/enabling the triggers)
+    action      :   [{
+        id          :   {type : String},
+        property    :   {type : String},
+        value       :   {type : String},
+        encoding    :   {type : String} // todo: how to think about this?
+    }],
+    trigger     :   TriggerSchema
+});
+
+/** Command **/
+var CommandSchema = new Mongoose.Schema({
+    command_id              :   {type: String, required: [true, 'Every command needs an id']},
+    command_string          :   {type: String},
+    command_params          :   [{
+        cmd_param   :   {type: String}, // param name, should match exactly with the one used in the command_string 'ab{cmd_param}\r'
+        default_val :   MixType,
+        param_min   :   {type: Number},
+        param_max   :   {type: Number}
+    }],
+    cmd_description         :   {type: String},
+    sample_response         :   {type: String},
+    sample_response_final   :   {type: String},
+    filter                  :   FilterSchema,
+    timeout                 :   {type: Number}, // if there is no timeout, global.timeout will be used
+    pre_process_func        :   {type: String},
+    post_process_func       :   {type: String},
+    priority                :   {type: Number, default: 3, enum: [1,2,3]} // todo: how many levels of priority?
 });
 
 /** Feed **/
@@ -61,29 +85,10 @@ var FeedSchema = new Mongoose.Schema({
     trigger                 : TriggerSchema
 });
 
-/** Filter **/
-var FilterSchema = new Mongoose.Schema({
-    regex       :   {type: String},
-    // todo: should action contain trigger? this way we can decide whether cycle complete should show up on the trigger modal.
-    // todo: (commonly known as the alert modal, but essentially user is creating/enabling the triggers)
-    action      :   [{
-        action_id   : String,
-        property    : String,
-        value       : String,
-        encoding    : {type : String}, // todo: how to think about this?
-        trigger     : TriggerSchema
-    }]
-});
-
-/** Command **/
-var CommandSchema = new Mongoose.Schema({
-    command_id              :   {type: String, required: [true, 'Every command needs an id']},
-    sample_response         :   {type: String},
-    sample_response_final   :   {type: String},
-    filter                  :   FilterSchema,
-    timeout                 :   {type: Number}, // if there is no timeout, global.timeout will be used
-    pre_process_func        :   {type: String},
-    priority                :   {type: Number, default: 3, enum: [1,2,3]} // todo: how many levels of priority?
+/** Sensor **/
+var SensorSchema = new Mongoose.Schema({
+    command             : CommandSchema,
+    interval            : {type: Number, default: 5, min: 0.8}
 });
 
 /** UI **/
@@ -98,6 +103,7 @@ var DriverSchema = new Mongoose.Schema({
     // ref to instrument model
     instrument      : {type: ObjectId, ref: 'Instrument'},
 
+    tOrg            : {type: String},
     communication : {
         serial      : SerialComSchema,
         bash        : ''     // todo: bash script and also allow bash script to take parameter
@@ -109,7 +115,7 @@ var DriverSchema = new Mongoose.Schema({
     sensors         :   [SensorSchema],
     feeds           :   [FeedSchema],
     commands        :   [CommandSchema],
-    unsolicited     :   {   // todo: how to combine unsolicited and command-driven, or it's ok to separate them for now
+    unsolicited     :   {   // todo: how to combine unsolicited and command-driven, or it's ok to separate them for now?
         sample_response_files   :   [{type:String}],
         filters                 :   [FilterSchema]
     },
